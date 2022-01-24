af2a00dc583cea8654fe62ed276c0b96df715da4
Zach <zachvaters@gmail.com>
2022-01-23 20:44:14 -0330

    Add test to validate typing test handler

--- app/pkg/handlers/response.go
+++ app/pkg/handlers/response.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 )
@@ -10,7 +9,6 @@ import (
 func apiResponse(status int, body interface{}, err error) (*events.APIGatewayProxyResponse, error) {
 	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
 	resp.StatusCode = status
-	log.Printf("API response: %v", body)
 	stringBody, _ := json.Marshal(body)
 	resp.Body = string(stringBody)
 	return &resp, err
--- tests/typingtest_test.go
+++ tests/typingtest_test.go
@@ -1,18 +1,55 @@
 package tests
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
 	handlers "github.com/zcvaters/quick-velritun/app/pkg/handlers"
 )
 
-func TestHandler(t *testing.T) {
-	t.Run("Unable to get IP", func(t *testing.T) {
-		res, err := handlers.TypingTest(events.APIGatewayProxyRequest{})
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Log(res)
-	})
+func TestTypingTestHandler(t *testing.T) {
+	type want struct {
+		Error     string
+		WordCount int
+	}
+
+	tests := []struct {
+		Want  want
+		Input handlers.TypingTestRequest
+	}{
+		{Input: handlers.TypingTestRequest{Words: -1}, Want: want{WordCount: 0, Error: "Failed to request words less than 1."}},
+		{Input: handlers.TypingTestRequest{Words: 5}, Want: want{WordCount: 5, Error: ""}},
+		{Input: handlers.TypingTestRequest{Words: 10}, Want: want{WordCount: 10, Error: ""}},
+		{Input: handlers.TypingTestRequest{Words: 15}, Want: want{WordCount: 15, Error: ""}},
+		{Input: handlers.TypingTestRequest{Words: 20}, Want: want{WordCount: 20, Error: ""}},
+		{Input: handlers.TypingTestRequest{Words: 25}, Want: want{WordCount: 0, Error: "Invalid request words must be less then 20."}},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("Words:%v", tc.Input.Words), func(t *testing.T) {
+			tcJSON, err := json.Marshal(tc.Input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			req := events.APIGatewayProxyRequest{
+				HTTPMethod: "POST",
+				Path:       "/typingtest",
+				Body:       string(tcJSON),
+			}
+			res, err := handlers.TypingTest(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var actual handlers.TypingTestResponse
+			json.Unmarshal([]byte(res.Body), &actual)
+			if len(actual.Words) != tc.Want.WordCount {
+				t.Fatalf("Expected word count %v got: %v", tc.Want.WordCount, len(actual.Words))
+			}
+			if actual.ErrorMsg != tc.Want.Error {
+				t.Fatalf("Expected error %v got: %v", tc.Want.Error, actual.ErrorMsg)
+			}
+		})
+	}
 }
